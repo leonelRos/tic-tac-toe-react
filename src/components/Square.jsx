9d0217f7d5c77869ff02c69db94a6cf69af80dbd
Leonel Rosales <rleonel426@gmail.com>
2020-08-22 21:34:18 -0700

    changing to function component

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 
 function Square(props) {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value: null
-  //   };
-  // }
-  // state = {
-  //   value : null,
-  // }
-
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
