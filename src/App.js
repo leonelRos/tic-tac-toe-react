3bbe20179048d81fe936784ddec113bfaba16c75
Leonel Rosales <rleonel426@gmail.com>
2020-08-30 21:30:55 -0700

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,23 +2,8 @@ import React from "react";
 import "./styles.css";
 
 class App extends React.Component {
-  state = {
-    name: "Mark",
-    age: 15
-  };
   render() {
-    return (
-      <div className="shopping-list">
-        <h1>
-          shopping list for {this.state.name} he is {this.state.age}
-        </h1>
-        <ul>
-          <li>Instagram</li>
-          <li>Facebook</li>
-          <li>Linkedin</li>
-        </ul>
-      </div>
-    );
+    return <div></div>;
   }
 }
 
